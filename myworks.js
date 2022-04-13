8a63bea2f5df8b64af09c2313bd253c3d5327ffa
AlazarG19 <alazargetachew70@gmail.com>
2022-04-13 04:36:24 -0700

    making the works mobile friendly

--- myworks.js
+++ myworks.js
@@ -1,43 +1,50 @@
 let images = document.querySelectorAll(".workimg")
+let visits = document.querySelectorAll(".btn-visit")
 let hoverMe = document.querySelector(".hoverme")
 let downButton = document.querySelectorAll(".fa-chevron-up")
 observer.observe(hoverMe)
-// downButton.forEach(
-//     (item)=>{
-//         observer.observe(item)
-    
+    // downButton.forEach(
+    //     (item)=>{
+    //         observer.observe(item)
+
 //     }
 // )
 downButton.forEach(
-    (item)=>{
+    (item) => {
         item.style.animation = `animateup 1.5s  infinite`
     }
 )
-images.forEach((image)=>{
-    image.addEventListener("click",()=>{
-        window.open(image.dataset.link,'_self')
+visits.forEach((visit) => {
+    visit.addEventListener("click", () => {
+        window.open(visit.dataset.link, '_self')
     })
-    image.addEventListener("mouseover",()=>{
+})
+images.forEach((image) => {
+    image.addEventListener("click", () => {
+        window.open(image.dataset.link, '_self')
+    })
+    image.addEventListener("mouseover", () => {
         images.forEach(element => {
-            if(element.dataset.name !== image.dataset.name){
+            if (element.dataset.name !== image.dataset.name) {
                 element.style.animation = "animation .7s linear forwards"
-            }else{
+            } else {
                 let des = document.querySelector(`.${image.dataset.name}d${image.dataset.n}`)
                 element.style.animation = "scale .7s forwards"
                 des.style.animation = "animation4 .7s .3s forwards"
             }
 
-       });
+        });
     })
-    image.addEventListener("mouseleave",()=>{
+    image.addEventListener("mouseleave", () => {
         images.forEach(element => {
-            if(element.dataset.name !== image.dataset.name){
+            if (element.dataset.name !== image.dataset.name) {
                 element.style.animation = "animation2 .7s forwards"
-            }else{
+            } else {
                 let des = document.querySelector(`.${image.dataset.name}d${image.dataset.n}`)
                 element.style.animation = "scale2 .7s forwards"
                 des.style.animation = "animation3 .1s forwards"
 
             }
-       });})
+        });
+    })
 })
